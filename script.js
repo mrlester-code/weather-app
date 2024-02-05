a6064fc03c840971ae25cd0a3c875c0c058eb1a1
Felipe Guarezi <83558188+mrlester-code@users.noreply.github.com>
2024-02-05 23:41:54 +1100

    Update script.js
    Adding some comments to the code

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// Variables holding the key and the link for the API
 const apiKey = "&appid=e069960c3b1c90dcada79b902adc6f0f";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
+//Variables to look for the clicks on the HTML page
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + apiKey);
-    
+  //Error message if there is a misspelled country/city  
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
@@ -15,12 +16,13 @@ async function checkWeather(city){
     var data = await response.json();
 
     //console.log(data);
-
+//Since we were printing the information on the console, now we basically look inside the information that The API give us
+//format to what aspects we want (metric)
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    
+    //Define all the weathers and images
     if(data.weather[0].main == "Clouds"){
         weatherIcon.src = "images/clouds.png";
     }else if(data.weather[0].main == "Clear"){
@@ -39,7 +41,7 @@ async function checkWeather(city){
     document.querySelector(".error").style.display = "none";
 }
 }
-
+//Since everything is locked we just need to read the click to show the values
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
 })
